0c3ed4b83aea87ebbef11f79888a8630b1662817
Chandra <chandra@ukirama.com>
2017-05-15 16:10:56 +0700

    Remove remove button from pokemon battle index and adding pokemon winner and pokemon loser column in pokemon battle index

--- app/decorators/pokemon_battle_decorator.rb
+++ app/decorators/pokemon_battle_decorator.rb
@@ -19,6 +19,8 @@ class PokemonBattleDecorator
     :current_hp_pokemon2,
     :link_to_pokemon1,
     :link_to_pokemon2,
+    :link_to_pokemon_winner,
+    :link_to_pokemon_loser,
     :link_to_edit,
     :link_to_remove
   )
@@ -47,7 +49,6 @@ class PokemonBattleDecorator
     if pokemon_battle.present?
 
       result = DecoratorResult.new
-
       result.pokemon1_id = pokemon_battle.pokemon1_id
       result.pokemon2_id = pokemon_battle.pokemon2_id
       result.pokemon1 = set_pokemon_name(pokemon_battle.pokemon1)
@@ -64,18 +65,40 @@ class PokemonBattleDecorator
       result.link_to_pokemon2 = set_link_to_pokemon2(pokemon_battle)
       result.link_to_edit = set_link_to_edit(pokemon_battle)
       result.link_to_remove = set_link_to_remove(pokemon_battle)
+      result.state = pokemon_battle.state
+
+      if pokemon_battle.pokemon_winner.present?
+        result.link_to_pokemon_winner = set_link_to_pokemon_winner_or_loser(Pokemon.find(pokemon_battle.pokemon_winner_id))
+        result.link_to_pokemon_loser = set_link_to_pokemon_winner_or_loser(Pokemon.find(pokemon_battle.pokemon_loser_id))
+      else
+        result.link_to_pokemon_winner = '-'
+        result.link_to_pokemon_loser = '-'
+      end
+
       result
     else
       nil
     end
   end
+
+  def set_pokemon_winner(pokemon_winner_id)
+    Pokemon.find(pokemon_winner_id).name
+  end
+
+  def set_pokemon_loser(pokemon_loser_id)
+    Pokemon.find(pokemon_loser_id).name
+  end
+
   def set_pokemon_name(pokemon)
     Pokemon.find(pokemon).name
   end
 
   def set_current_hp(pokemon)
-
     pokemon.current_health_point.to_s + ' / ' + pokemon.max_health_point.to_s
+  end
+
+  def set_link_to_pokemon_winner_or_loser(pokemon)
+    @context.helpers.link_to pokemon.name, pokemon
   end
 
   def set_link_to_pokemon1(pokemon_battle)
